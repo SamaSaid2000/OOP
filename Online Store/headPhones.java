ec4d3a62ff5e5b086c38e5fc5fe638ca71962608
SamaSaid2000 <84671207+SamaSaid2000@users.noreply.github.com>
2021-05-24 19:03:51 +0200

    Add files via upload

---
+++ Online Store/headPhones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jumia;
+
+/**
+ *
+ * @author sama
+ */
+public class headPhones extends electronics {
+    
+    
+    public headPhones (){}
+    
+    
+     String brand;
+     
+    
+}
