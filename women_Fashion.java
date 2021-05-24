50b4fe79625237955af02caa21deff38e9f5b11f
SamaSaid2000 <84671207+SamaSaid2000@users.noreply.github.com>
2021-05-24 19:02:50 +0200

    Add files via upload

---
+++ women_Fashion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jumia;
+
+/**
+ *
+ * @author sama
+ */
+public class women_Fashion extends Fashion {
+    
+    public women_Fashion (){}
+    
+    String type;
+    
+    
+}
